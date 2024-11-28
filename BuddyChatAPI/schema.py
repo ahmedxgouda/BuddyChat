33a12a37ee68efc8b7853fe3a26aab141a626272
Ahmed Gouda <ahmedmosad405@gmail.com>
2024-11-28 17:05:44 +0200

    Add GraphQL types for models and update query fields

--- BuddyChatAPI/schema.py
+++ BuddyChatAPI/schema.py
@@ -1,10 +1,58 @@
 import graphene
+from graphene_django import DjangoObjectType, DjangoListField
+from .models import *
 
+class CustomUserType(DjangoObjectType):
+    class Meta:
+        model = CustomUser
+        fields = "__all__"
+        
+class MessageType(DjangoObjectType):
+    class Meta:
+        model = Message
+        fields = ('id', 'sender', 'receiver', 'content', 'date', 'read_at')
+        
+class ChatType(DjangoObjectType):
+    class Meta:
+        model = Chat
+        fields = "__all__"
+        
+class ChatMessageType(DjangoObjectType):
+    class Meta:
+        model = ChatMessage
+        fields = "__all__"
+        
+class UserGroupType(DjangoObjectType):
+    class Meta:
+        model = UserGroup
+        fields = "__all__"
+        
+class GroupMessageType(DjangoObjectType):
+    class Meta:
+        model = GroupMessage
+        fields = "__all__"
+        
+class GroupMemberType(DjangoObjectType):
+    class Meta:
+        model = GroupMember
+        fields = "__all__"
+        
+class AttachmentType(DjangoObjectType):
+    class Meta:
+        model = Attachment
+        fields = "__all__"
+        
+class NotificationType(DjangoObjectType):
+    class Meta:
+        model = Notification
+        fields = "__all__"
+    
 class Query(graphene.ObjectType):
-    hello = graphene.String(name=graphene.String(default_value="stranger"))
-
-    def resolve_hello(self, info, name):
-        return f"Hello, {name}!"
+    users = DjangoListField(CustomUserType)
+    messages = DjangoListField(MessageType)
+    chats = DjangoListField(ChatType)
+    user_groups = DjangoListField(UserGroupType)
+    notifications = DjangoListField(NotificationType)
     
 schema = graphene.Schema(query=Query)
 
