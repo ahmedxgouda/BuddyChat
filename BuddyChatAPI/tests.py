947b4a7314f62496c6c64de2b474d2f93c88dbfa
Ahmed Gouda <ahmedmosad405@gmail.com>
2024-12-02 11:13:03 +0200

    Add GraphQL tests for user creation and validation; include checks for invalid inputs

--- BuddyChatAPI/tests.py
+++ BuddyChatAPI/tests.py
@@ -1,3 +1,158 @@
-from django.test import TestCase
-
+from .models import *
+from graphene_django.utils.testing import GraphQLTestCase
 # Create your tests here.
+
+class UserTestCase(GraphQLTestCase):
+    def setUp(self):
+        self.user1 = CustomUser.objects.create_user(username='test', first_name='test', last_name='test', password="123456789Test", email="test@buddy-chat.com", phone="+201234567890")
+        self.user2 = CustomUser.objects.create_user(username='test1', first_name='test1', last_name='test1', password="113456789Test", email="test1@buddy-chat.com", phone="+201234567891")
+        self.user1.save()
+        self.user2.save()
+        # Repeated mutation for testing
+        self.create_user_mutation = '''
+            mutation CreateUser($username: String!, $email: String!, $password: String!, $phone: String!, $firstName: String!, $lastName: String!) {
+                createUser(username: $username, email: $email, password: $password, phone: $phone, firstName: $firstName, lastName: $lastName) {
+                    user {
+                        id
+                        username
+                        email
+                        phone
+                        firstName
+                        lastName
+                    }
+                }
+            }
+        '''
+        
+    def test_query_users(self):
+        response = self.query(
+            '''
+            query {
+                users {
+                    edges {
+                        node {
+                            id
+                            username
+                            email
+                            phone
+                            firstName
+                            lastName
+                        }
+                    }
+                }
+            }
+            '''
+        )
+        content = response.json()
+        self.assertResponseNoErrors(response)
+        self.assertEqual(len(content['data']['users']['edges']), 2)
+        self.assertEqual(content['data']['users']['edges'][1]['node']['username'], 'test')
+    
+    def test_query_user(self):
+        query = '''
+            query user($id: Int!) {
+                user(id: $id) {
+                    id
+                    username
+                    email
+                    phone
+                    firstName
+                    lastName
+                }
+            }
+        '''
+        response = self.query(query=query, variables={'id': self.user1.id})
+        content = response.json()
+        self.assertResponseNoErrors(response)
+        self.assertEqual(content['data']['user']['username'], 'test')
+        
+    def test_create_user_invalid_email(self):
+        response = self.query(query=self.create_user_mutation, variables={'username': 'test2', 'email': 'a@g', 'password': '123456789Test', 'phone': '+201234567897', 'firstName': 'test2', 'lastName': 'test2'})
+        
+        content = response.json()
+        self.assertIn("errors", content)
+    
+    def test_create_user_invalid_phone(self):
+        response = self.query(query=self.create_user_mutation, variables={'username': 'test2', 'email': 'a@g.com', 'password': '123456789Test', 'phone': '+20123456789', 'firstName': 'test2', 'lastName': 'test2'})
+        
+        content = response.json()
+        self.assertIn("errors", content)
+        
+    def test_create_user_invalid_password_letters_only(self):
+        response = self.query(
+            query=self.create_user_mutation, 
+            variables={'username': 'test2', 'email': 'a@g.com', 'password': 'aaaaaa', 'phone': '+201234567897', 'firstName': 'test2', 'lastName': 'test2'}
+        )
+        
+        content = response.json()
+        self.assertIn("errors", content)
+        
+    def test_create_user_invalid_password_numbers_only(self):
+        response = self.query(
+            query=self.create_user_mutation, 
+            variables={'username': 'test2', 'email': 'a@f.com', 'password': '123456789', 'phone': '+201234567897', 'firstName': 'test2', 'lastName': 'test2'}
+        )
+        
+        content = response.json()
+        self.assertIn("errors", content)
+        
+    def test_create_user_invalid_password_no_uppercase(self):
+        response = self.query(
+            query=self.create_user_mutation, 
+            variables={'username': 'test2', 'email': 'a@f.com', 'password': '123456789test', 'phone': '+201234567897', 'firstName': 'test2', 'lastName': 'test2'}
+        )
+        
+        content = response.json()
+        self.assertIn("errors", content)
+        
+    def test_create_user_invalid_password_no_lowercase(self):
+        response = self.query(
+            query=self.create_user_mutation, 
+            variables={'username': 'test2', 'email': 'a@f.com', 'password': '123456789TEST', 'phone': '+201234567897', 'firstName': 'test2', 'lastName': 'test2'}
+        )
+        
+        content = response.json()
+        self.assertIn("errors", content)
+        
+        
+    def test_create_user_invalid_username(self):
+        response = self.query(
+            query=self.create_user_mutation, 
+            variables={'username': 't', 'email': 'a@f.com', 'password': '123456789Test', 'phone': '+201234567897', 'firstName': 'test2', 'lastName': 'test2'}
+        )
+        
+        content = response.json()
+        self.assertIn("errors", content)
+        
+    def test_create_user_invalid_first_name(self):
+        response = self.query(
+            query=self.create_user_mutation, 
+            variables={'username': 'test2', 'email': 'a@f.com', 'password': '123456789Test', 'phone': '+201234567897', 'firstName': 't', 'lastName': 'test2'}
+        )
+        
+        content = response.json()
+        self.assertIn("errors", content)
+        
+    def test_create_user_invalid_last_name(self):
+        response = self.query(
+            query=self.create_user_mutation, 
+            variables={'username': 'test2', 'email': 'a@f.com', 'password': '123456789Test', 'phone': '+201234567897', 'firstName': 'test2', 'lastName': 't'}
+        )
+        
+        content = response.json()
+        self.assertIn("errors", content)
+        
+    def test_create_user(self):
+        response = self.query(
+            query=self.create_user_mutation, 
+            variables={'username': 'test2', 'email': 'a@f.com', 'password': '123456789Test', 'phone': '+201234567897', 'firstName': 'test2', 'lastName': 'test2'}
+        )
+        
+        content = response.json()
+        self.assertResponseNoErrors(response)
+        self.assertEqual(content['data']['createUser']['user']['username'], 'test2')
+        self.assertEqual(content['data']['createUser']['user']['email'], 'a@f.com')
+        self.assertEqual(content['data']['createUser']['user']['phone'], '+201234567897')
+        self.assertEqual(content['data']['createUser']['user']['firstName'], 'test2')
+        self.assertEqual(content['data']['createUser']['user']['lastName'], 'test2')
+        
