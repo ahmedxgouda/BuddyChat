ff7da3f0e7e1f83c465aa0264dba2381fe20a0a5
Ahmed Gouda <ahmedmosad405@gmail.com>
2024-12-02 10:06:24 +0200

    Add password validation and enhance user data validation; improve error messages

--- BuddyChatAPI/GraphQL/validators.py
+++ BuddyChatAPI/GraphQL/validators.py
@@ -1,22 +1,42 @@
 from django.core.exceptions import ValidationError
 from ..models import CustomUser, Chat
+from django.core.validators import validate_email
+
+def validate_password(password):
+    if len(password) < 8:
+        raise ValidationError('Password must be at least 8 characters long')
+    if password.isalpha():
+        raise ValidationError('Password must contain at least one digit')
+    if password.isdigit():
+        raise ValidationError('Password must contain at least one letter')
+    if password.islower():
+        raise ValidationError('Password must contain at least one uppercase letter')
+    if password.isupper():
+        raise ValidationError('Password must contain at least one lowercase letter')
+    return True
 
 def validate_user_data(username, email, password, phone, first_name, last_name):
     
     if CustomUser.objects.filter(username=username).exists():
-        raise ValidationError('Username already exists')
+        raise ValidationError(f'Username: {username} already exists')
     if CustomUser.objects.filter(email=email).exists():
-        raise ValidationError('Email already exists')
+        raise ValidationError(f'Email: {email} already exists')
     if CustomUser.objects.filter(phone=phone).exists():
-        raise ValidationError('Phone number already exists')
-    if len(password) < 8:
-        raise ValidationError('Password must be at least 8 characters long')
+        raise ValidationError(f'Phone number: {phone} already exists')
+    try:
+        validate_email(email)
+    except ValidationError:
+        raise ValidationError(f'Invalid email: {email}')
+    
+    if len(username) < 4:
+        raise ValidationError('Username must be at least 4 characters long')
     if len(phone) != 13:
         raise ValidationError('Phone number must be 13 characters long')
     if len(first_name) < 2:
         raise ValidationError('First name must be at least 2 characters long')
     if len(last_name) < 2:
         raise ValidationError('Last name must be at least 2 characters long')
+    validate_password(password)
     return True
 
 def validate_message_content(content):
@@ -27,6 +47,7 @@ def validate_message_content(content):
 def validate_chat_users(user1, user2):
     if Chat.objects.filter(user1=user1, user2=user2).exists() or Chat.objects.filter(user1=user2, user2=user1).exists():
         raise ValidationError('Chat already exists')
+    return True
     
 def validate_group_member(user_group, member):
     if user_group.members.filter(pk=member.pk).exists():
