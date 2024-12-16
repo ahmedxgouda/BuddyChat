44002c9ce4ffc499018ffd8a66b0d2a6fc85ef8b
Ahmed Gouda <ahmedmosad405@gmail.com>
2024-12-16 11:01:24 +0200

    Refactor SetNotificationAsRead mutation to use global ID for notification lookup

--- BuddyChatAPI/GraphQL/mutations/notification_mutations.py
+++ BuddyChatAPI/GraphQL/mutations/notification_mutations.py
@@ -1,17 +1,16 @@
 import graphene
-from django.shortcuts import get_object_or_404
 from graphql_jwt.decorators import login_required
-from ...models import Notification
 from ..types import NotificationType
+from graphene.relay.node import Node
 
 class SetNotificationAsRead(graphene.Mutation):
     class Arguments:
-        notification_id = graphene.Int()
+        notification_id = graphene.ID()
         
     notification = graphene.Field(NotificationType)
     
     @login_required
     def mutate(self, info, notification_id):
-        notification = get_object_or_404(Notification, pk=notification_id)
+        notification = Node.get_node_from_global_id(info, notification_id)
         notification.is_read = True
         return SetNotificationAsRead(notification=notification)
